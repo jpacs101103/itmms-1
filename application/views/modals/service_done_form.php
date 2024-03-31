603180f55e78e1801f960e12405dcd275e194fa9
Andrew Oplas <andrewmoplas@gmail.com>
2024-03-31 15:05:41 +0800

    update replacement in service done report

--- application/views/modals/service_done_form.php
+++ application/views/modals/service_done_form.php
@@ -5,6 +5,8 @@
     </div>
     <form method="post" class="form-horizontal">
         <div class="modal-body">
+            <input type="hidden" name="complaint" id="complaint" />
+
             <div class="form-group">
                 <div class="col-sm-12 itmms-label">Reference Number:<i class="fa fa-lock fg-gray pull-right padding-right5"></i></div>
                 <div class="col-sm-12">
@@ -18,12 +20,7 @@
                     <div class="col-sm-9">
                         <div class="inner-addon right-addon">
                             <i class="fa fa-calendar"></i>
-                            <input type='text'
-                                   class="datepicker-here form-control"
-                                   id="date_finished"
-                                   name="date_finished"
-                                   readonly
-                            />
+                            <input type='text' class="datepicker-here form-control" id="date_finished" name="date_finished" readonly />
                         </div>
                     </div>
                 </div>
@@ -33,11 +30,7 @@
                     <div class="col-sm-9">
                         <div class="inner-addon right-addon">
                             <i class="fa fa-clock-o"></i>
-                            <input id="time_finished"
-                                   type="text"
-                                   name="time_finished"
-                                   class="form-control"
-                            />
+                            <input id="time_finished" type="text" name="time_finished" class="form-control" />
                         </div>
                     </div>
                 </div>
@@ -48,12 +41,7 @@
                     <div class="col-sm-9">
                         <div class="inner-addon right-addon">
                             <i class="fa fa-calendar"></i>
-                            <input type='text'
-                                   class="datepicker-here form-control"
-                                   id="date_replaced"
-                                   name="date_replaced"
-                                   readonly
-                            />
+                            <input type='text' class="datepicker-here form-control" id="date_replaced" name="date_replaced" readonly />
                         </div>
                     </div>
                 </div>
@@ -63,11 +51,7 @@
                     <div class="col-sm-9">
                         <div class="inner-addon right-addon">
                             <i class="fa fa-clock-o"></i>
-                            <input id="time_replaced"
-                                   type="text"
-                                   name="time_replaced"
-                                   class="form-control"
-                            />
+                            <input id="time_replaced" type="text" name="time_replaced" class="form-control" />
                         </div>
                     </div>
                 </div>
@@ -82,20 +66,38 @@
             <div class="form-group">
                 <div class="col-sm-12 itmms-label">Unit Status:</div>
                 <div class="col-sm-12">
-                     <select class="form-control" name="unit_status" id="unit_status">
+                    <select class="form-control" name="unit_status" id="unit_status">
                         <option value="">Select Unit Status</option>
                         <option value="repaired">Repaired</option>
                         <option value="under warranty">Replacement</option>
                     </select>
                 </div>
             </div>
+
+            <div class="form-group hidden computer-parts-field">
+                <div class="col-sm-12">
+                    <span class="itmms-label" for="computer_part">Part: </span>
+                    <select class="form-control" name="computer_part" id="computer_part">
+                        <option selected disabled>Select Part</option>
+                        <option value="Monitor">Monitor</option>
+                        <option value="Hard Drive">Hard Drive</option>
+                        <option value="Motherboard">Motherboard</option>
+                        <option value="Power Supply">Power Supply</option>
+                        <option value="Processor">Processor</option>
+                        <option value="RAM">RAM</option>
+                        <option value="Video Card">Video Card</option>
+                        <option value="Printer">Printer</option>
+                    </select>
+                </div>
+            </div>
+
             <div class="form-group">
                 <div class="col-sm-12 itmms-label">Diagnose / Troubleshooting Report:</div>
                 <div class="col-sm-12">
-                      <textarea class="form-control" rows="5" name="action_taken" placeholder="Type here ..." id="action_taken"></textarea>
+                    <textarea class="form-control" rows="5" name="action_taken" placeholder="Type here ..." id="action_taken"></textarea>
                 </div>
             </div>
-<!--             <div class="form-group" id="return">
+            <!--             <div class="form-group" id="return">
                 <div class="col-sm-12 itmms-label">Received By:</div>
                 <div class="col-sm-12">
                       <input type="text" class="form-control" name="returned_to" id="returned_to" placeholder="Input Returned to Personnel" />
@@ -110,7 +112,7 @@
                     <div class="inner-addon right-addon">
                         <div class="col-sm-6 margin-negative-left">Date Resolved:</div>
                         <i class="fa fa-calendar margin-top"></i>
-                        <input type='text' class="form-control" id="property_date_received" name="property_date_received" placeholder="Input Date Replaced"/>
+                        <input type='text' class="form-control" id="property_date_received" name="property_date_received" placeholder="Input Date Replaced" />
                     </div>
                 </div>
             </div>
@@ -121,4 +123,3 @@
         </div>
     </form>
 </div>
-
